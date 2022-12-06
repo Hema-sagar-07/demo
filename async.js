a19f531619c51ce05163050facbfc46222b62468
Hema-sagar-07 <119473211+Hema-sagar-07@users.noreply.github.com>
2022-12-06 18:06:32 +0530

    Add files via upload
    initial commit

---
+++ async.js
@@ -0,0 +1,41 @@
+import fetch from 'node-fetch'
+// async function fetching(){
+
+//     const res =    fetch('https://jsonplaceholder.typicode.com/photos');
+//     const photos =  res.json();
+
+//     console.log(photos)
+//     console.log(1);
+//  }
+
+// fetching()
+// setTimeout(()=>{
+//     console.log("4");
+// },4)
+// setTimeout(()=>{
+//     console.log("3");
+// },3);setTimeout(()=>{
+//     console.log("3");
+// },3);setTimeout(()=>{
+//     console.log("3");
+// },3);setTimeout(()=>{
+//     console.log("3");
+// },3);setTimeout(()=>{
+//     console.log("2");
+// },2);
+
+fetch('https://jsonplaceholder.typicode.com/photos')
+    .then(r => r.json())
+    .then(r => console.log(r))
+
+try {
+
+} catch {
+
+} finally {
+
+}
+
+
+
+// console.log("hi");
